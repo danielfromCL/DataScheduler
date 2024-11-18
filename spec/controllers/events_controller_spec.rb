867d8ca9d4ff415f864de84739896cddd8a7ecbc
danielfromCL <danielngc1@gmail.com>
2024-11-18 15:37:24 -0300

    test(Controllers): add files for testing controller methods

---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe EventsController do
+  describe "GET" do
+    it "" do
+    end
+  end
+  describe 'POST' do
+
+  end
+  describe 'PATCH' do
+
+  end
+  describe 'DELETE' do
+
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe UsersController do
+  describe "GET" do
+    it "" do
+    end
+  end
+  describe 'POST' do
+
+  end
+  describe 'PATCH' do
+
+  end
+  describe 'DELETE' do
+
+  end
+end
