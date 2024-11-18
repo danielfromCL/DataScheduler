c89c1f1389a2c3b3d4b304dc06288985bcb5a794
danielfromCL <danielngc1@gmail.com>
2024-11-18 17:48:03 -0300

    feat(Event): add model validations, and add association between event and eventparticipant

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :event_participants
+  validates :from_date, presence: true
+  validates :to_date, presence: true
 end
--- app/models/event_participant.rb
+++ app/models/event_participant.rb
@@ -1,2 +1,4 @@
 class EventParticipant < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
 end
