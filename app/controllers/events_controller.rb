c4ccb65fb51a7f2fed2e432cce533f46f4e980c5
danielfromCL <danielngc1@gmail.com>
2024-11-19 14:44:11 -0300

    feat(EventsController): add create endpoint

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,13 @@
 class EventsController < ApplicationController
+  before_action :validate_participant, only: :create
   def index
     @events = get_user.events_ordered_by_from_date
     render json: @events, status: :ok
+  end
+
+  def create
+    @event = Event.create!(**create_params)
+    render json: @event, status: :created
   end
 
   private
@@ -12,5 +18,13 @@ class EventsController < ApplicationController
     else
       @user
     end
+  end
+
+  def create_params
+    params.permit(:name, :from_date, :to_date, :online, :city, :country, :state, :url)
+  end
+
+  def validate_participant
+    render status: :forbidden if @user.role != 'owner' || @user.company_id != get_user.company_id
   end
 end
