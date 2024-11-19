0a8861481c03c46e34bec97946e7df0fba721ca6
danielfromCL <danielngc1@gmail.com>
2024-11-19 20:14:26 -0300

    feat(OpenWeather): implement API for getting weather

---
+++ config/initializers/open_weather.rb
@@ -0,0 +1,28 @@
+class OpenWeatherAPI
+  def initialize
+    @api_key = ENV['SENDGRID_API_KEY']
+  end
+
+  def get_weather(city, state, country, from)
+    place = get_coordinates(city, state, country)
+    resp = JSON.parse(RestClient.get("https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=#{place[:lat]}&lon=#{place[:lon]}&dt=#{from.to_time.to_i}&units=metric&appid=#{@api_key}"))
+    {
+      main: resp['data'][0]['weather'][0]['main'],
+      description: resp['data'][0]['weather'][0]['description'],
+      temperature: resp['data']['temp']
+    }
+  end
+
+  def get_coordinates(city, state, country)
+    place = city
+    place += ",#{state}" if state
+    place += ",#{country}" if country
+    resp = JSON.parse(RestClient.get("http://api.openweathermap.org/geo/1.0/direct?q=#{place}&limit=1&appid=#{@api_key}"))
+    {
+      lat: resp['lat'],
+      lon: resp['lon']
+    }
+  end
+end
+
+OpenWeather = OpenWeatherAPI.new
