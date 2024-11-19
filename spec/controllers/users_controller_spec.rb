15d0a28619c5a47685d4533d53b522dd14589b12
danielfromCL <danielngc1@gmail.com>
2024-11-19 15:31:27 -0300

    test(UsersController): add test for obtaining companies' users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,22 @@ require 'rails_helper'
 
 RSpec.describe UsersController do
   describe "GET" do
-    it "" do
+    let!(:company) { FactoryBot.create(:company) }
+    let!(:owner) { FactoryBot.create(:user, company:, role: 'owner') }
+    let!(:member_1) { FactoryBot.create(:user, company:) }
+    let!(:member_2) { FactoryBot.create(:user, company:) }
+    let!(:outsider) { FactoryBot.create(:user) }
+
+    it "lists all of a companies' users if owner" do
+      request.headers['Authorization'] = authenticate(owner)
+      get :index,
+          params: {
+            company_id: company.id
+          }
+      expect(response).to have_http_status(:ok)
+      body = JSON.parse(response.body)
+      expect(body.length).to eq(3)
+      expect(body.map { |m| m['id']}).to match_array([owner.id, member_1.id, member_2.id])
     end
   end
   describe 'POST' do
