f02c71731de4546a29e9b2025c8919dca4bedb4a
danielfromCL <danielngc1@gmail.com>
2024-11-19 20:13:55 -0300

    feat(EventsController): add weather to body response if event not online

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ApplicationRecord
   has_many :users, through: :event_participants
   validates :from_date, presence: true
   validates :to_date, presence: true
+
+  def weather
+    return if online || !city
+    OpenWeather.get_weather(city, state, country, from_date)
+  end
 end
