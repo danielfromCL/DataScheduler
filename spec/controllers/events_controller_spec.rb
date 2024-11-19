a24d26bdcfa7fff4adb441bc0a29701b88176c6d
danielfromCL <danielngc1@gmail.com>
2024-11-19 14:04:02 -0300

    test(EventsController): add new expects for seeing all of the event's relevant data

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe EventsController, type: :controller do
 
     it "lists own events in chronological order" do
       event_1 = FactoryBot.create(:event, from_date: Date.yesterday)
-      event_2 = FactoryBot.create(:event, from_date: Date.today)
+      event_2 = FactoryBot.create(:event, from_date: Date.today, online: false, city: 'Santiago', country: 'CL')
       event_3 = FactoryBot.create(:event, from_date: Date.yesterday - 1, to_date: Date.yesterday)
       event_4 = FactoryBot.create(:event, from_date: Date.yesterday)
       participant_1 = FactoryBot.create(:event_participant, event: event_1, user:)
@@ -30,11 +30,23 @@ RSpec.describe EventsController, type: :controller do
       expect(DateTime.parse(resp[0]['from_date'])).to eq(event_3.from_date)
       expect(DateTime.parse(resp[1]['from_date'])).to eq(event_1.from_date)
       expect(DateTime.parse(resp[2]['from_date'])).to eq(event_2.from_date)
+      expect(resp[0]['online']).to eq(true)
+      expect(resp[1]['online']).to eq(true)
+      expect(resp[2]['online']).to eq(false)
+      expect(resp[0]['city']).to be_nil
+      expect(resp[1]['city']).to be_nil
+      expect(resp[2]['city']).to eq('Santiago')
+      expect(resp[0]['state']).to be_nil
+      expect(resp[1]['state']).to be_nil
+      expect(resp[2]['state']).to be_nil
+      expect(resp[0]['country']).to be_nil
+      expect(resp[1]['country']).to be_nil
+      expect(resp[2]['country']).to eq('CL')
     end
 
     it 'lists own events if no user_id provided' do
       event_1 = FactoryBot.create(:event, from_date: Date.yesterday)
-      event_2 = FactoryBot.create(:event, from_date: Date.today)
+      event_2 = FactoryBot.create(:event, from_date: Date.today, online: false, city: 'Santiago', country: 'CL')
       event_3 = FactoryBot.create(:event, from_date: Date.yesterday - 1, to_date: Date.yesterday)
       event_4 = FactoryBot.create(:event, from_date: Date.yesterday)
       participant_1 = FactoryBot.create(:event_participant, event: event_1, user:)
@@ -52,6 +64,18 @@ RSpec.describe EventsController, type: :controller do
       expect(DateTime.parse(resp[0]['from_date'])).to eq(event_3.from_date)
       expect(DateTime.parse(resp[1]['from_date'])).to eq(event_1.from_date)
       expect(DateTime.parse(resp[2]['from_date'])).to eq(event_2.from_date)
+      expect(resp[0]['online']).to eq(true)
+      expect(resp[1]['online']).to eq(true)
+      expect(resp[2]['online']).to eq(false)
+      expect(resp[0]['city']).to be_nil
+      expect(resp[1]['city']).to be_nil
+      expect(resp[2]['city']).to eq('Santiago')
+      expect(resp[0]['state']).to be_nil
+      expect(resp[1]['state']).to be_nil
+      expect(resp[2]['state']).to be_nil
+      expect(resp[0]['country']).to be_nil
+      expect(resp[1]['country']).to be_nil
+      expect(resp[2]['country']).to eq('CL')
     end
 
     it 'does not returns a list if no authentication provided' do
